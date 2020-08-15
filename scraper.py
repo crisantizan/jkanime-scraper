1ebc862844500b93e064eace6587d30ea3f23b46
Cristian Santiz <crisantizan@gmail.com>
2020-08-15 13:22:47 -0500

    feat: write in file

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import TimeoutException, ElementClickIntercepted
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import re
 
 LINK = 'https://jkanime.net/the-god-of-high-school'
 LINK_2 = 'https://jkanime.net/major-2nd-tv-2nd-season'
@@ -57,36 +58,43 @@ def fetch(browser, url, xpath, sleep=0):
         time.sleep(sleep)
 
     return scrape(browser, xpath)
-    #  # get the innerhtml from the rendered page
-    # innerHTML = browser.execute_script("return document.body.innerHTML")
-    # # Now use lxml to parse the page
-    # parsed = html.fromstring(innerHTML)
-
-    # # Get your element with xpath
-    # return parsed.xpath(xpath)
 
 
-def input_link():
+def input_data():
     link = input('Enter link: ')
+    embed = input('Do you want get embed url\'s? [y]es - [n]o: ')[0]
+
+    get_embed = embed.lower() == 'y'
 
     # remove last /
     if link[-1:] == '/':
-        return link[0:-1]
+        return {'link': link[0:-1], 'embed': get_embed}
 
-    return link
+    return {'link': link, 'embed': get_embed}
 
 
 if __name__ == '__main__':
-    link = input_link()
+    input_data = input_data()
+
+    link = input_data['link']
+    embed = input_data['embed']
+    title = re.sub(r'https://jkanime.net/', '', link)
+
     driver = init_driver()
     links = get_links(browser=driver, link=link)
 
-    for link in links:
-        print(link)
-    # links = fetch(browser=driver, url=LINK, xpath=XPATH_GET_LINKS, sleep=1)
+    for index, link in enumerate(links):
+        iframe_link = fetch(browser=driver, url=link,
+                            xpath=XPATH_IFRAME, sleep=1)[0]
+        src = fetch(browser=driver, url=iframe_link, xpath=XPATH_SOURCE)[0]
 
-    # for link in links:
-    #     iframe_link = fetch(browser=driver, url=link,
-    #                         xpath=XPATH_IFRAME, sleep=1)[0]
-    #     src = fetch(browser=driver, url=iframe_link, xpath=XPATH_SOURCE)[0]
-    #     print(src)
+        with open(f'{title}.txt', mode='a', encoding='utf-8') as f:
+            f.write('**** **** **** **** **** **** **** **** **** ****\n')
+            f.write(f'EPISODE {index + 1}:\n')
+            f.write(f'  LINK: {src}\n')
+            f.write(f'  EMBED: {iframe_link}\n')
+            f.write('**** **** **** **** **** **** **** **** **** ****\n\n')
+
+        print(f'Episode {index + 1} done')
+
+    driver.quit()
