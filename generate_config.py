ca67434a6145f923facc6b28f42d8c652f165d42
Cristian Santiz <crisantizan@gmail.com>
2020-08-18 23:27:44 -0500

    feat: validations

--- generate_config.py
+++ generate_config.py
@@ -4,20 +4,37 @@ from pathlib import Path
 import os
 
 
-def get_input_data(placeholder, is_path=False):
+def get_input_data(placeholder, cb=None):
     while True:
         data = input(placeholder)
         if not data:
             print('Please, fill this param')
         else:
-            if is_path and not helper.path_exists(path=data):
-                print('Path doesn\'t exists')
+            if type(cb).__name__ == 'function' and not cb(data):
                 continue
+
             return data
 
 
 def generate_filename(name):
     return f"{name.lower().replace(' ', '-')}.config.txt"
+
+
+def validate_output_folder(path):
+    if not helper.path_exists(path):
+        print('Path doesn\'t exists')
+        return False
+    else:
+        return True
+
+
+def validate_anime_txt(name):
+    path = os.path.join('animes', f'{name}.txt')
+    if not helper.path_exists(path):
+        print(f'Anime "{name}" has not been scraped')
+        return False
+    else:
+        return True
 
 
 def write_file(anime_txt, output_folder, anime_name):
@@ -33,8 +50,8 @@ def write_file(anime_txt, output_folder, anime_name):
 
 
 def main():
-    anime_txt = get_input_data('Anime file name (.txt file): ')
-    output_folder = get_input_data('Output folder: ', is_path=True)
+    anime_txt = get_input_data('.txt filename: ', validate_anime_txt)
+    output_folder = get_input_data('Output folder: ', validate_output_folder)
     anime_name = get_input_data('Anime name: ')
     write_file(anime_txt, output_folder, anime_name)
 
---
+++ t.py
@@ -0,0 +1,6 @@
+def hey():
+  return 'hey'
+
+
+if __name__ == "__main__":
+    print(type(hey).__name__ == 'function')
